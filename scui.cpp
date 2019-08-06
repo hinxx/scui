4d1cfd91e13732fa099e144619190032bde04b2d
Hinko Kocevar <hinko.kocevar@esss.se>
2019-08-06 13:55:02 +0200

    minor refactoring

--- scui.cpp
+++ scui.cpp
@@ -334,8 +334,8 @@ LONG sc_write_card(const SCARDHANDLE handle, BYTE address, LPBYTE data, BYTE len
  */
 
 
-static SCARDCONTEXT l_context = 0;
-static pthread_t l_thread_id = 0;
+static SCARDCONTEXT detect_context = 0;
+static pthread_t detect_thread_id = 0;
 
 /**
  * Thread routine which waits for change in reader and card state. 
@@ -346,34 +346,34 @@ static void *sc_detect_thread_fnc(void *ptr)
 {
     TRC("Enter\n");
 
-    LONG rv = sc_create_context(&l_context);
-    DBG("create CONTEXT 0x%08lX\n", l_context);
+    LONG rv = sc_create_context(&detect_context);
+    DBG("create CONTEXT 0x%08lX\n", detect_context);
     assert(rv == SCARD_S_SUCCESS);
 
     while (1) {
         TRC("loop, waiting for reader to arrive or leave ..\n");
         (void)fflush(stdout);
 
-        rv = sc_detect_reader(l_context);
+        rv = sc_detect_reader(detect_context);
         BOOL have_reader = sc_is_reader_attached();
         if (have_reader) {
             DBG("READER %s\n", sc_get_reader_name());
             DBG("probing for card..\n");
-            sc_probe_for_card(l_context);
+            sc_probe_for_card(detect_context);
             BOOL have_card = sc_is_card_inserted();
             if (have_card) {
                 DBG("CARD PRESENT..\n");
                 DBG("waiting for card remove..\n");
-                rv = sc_wait_for_card_remove(l_context);
+                rv = sc_wait_for_card_remove(detect_context);
             } else {
                 DBG("NO CARD!\n");
                 DBG("waiting for card insert..\n");
-                rv = sc_wait_for_card_insert(l_context);
+                rv = sc_wait_for_card_insert(detect_context);
             }
         } else {
             DBG("NO READER\n");
             DBG("waiting for reader..\n");
-            rv = sc_wait_for_reader(l_context, INFINITE);
+            rv = sc_wait_for_reader(detect_context, INFINITE);
         }
 
         // if the wait was cancelled (i.e. exiting the app), exit the thread!
@@ -383,8 +383,8 @@ static void *sc_detect_thread_fnc(void *ptr)
         }
     }
 
-    DBG("destroy CONTEXT 0x%08lX\n", l_context);
-    sc_destroy_context(&l_context);
+    DBG("destroy CONTEXT 0x%08lX\n", detect_context);
+    sc_destroy_context(&detect_context);
 
     TRC("Leave\n");
     return 0;
@@ -398,7 +398,7 @@ bool sc_detect_thread_start()
 {
     TRC("Enter\n");
 
-    int rv = pthread_create(&l_thread_id, NULL, sc_detect_thread_fnc, NULL);
+    int rv = pthread_create(&detect_thread_id, NULL, sc_detect_thread_fnc, NULL);
     if (rv) {
         ERR("Error - pthread_create() return code: %d\n", rv);
         TRC("Leave false\n");
@@ -418,11 +418,11 @@ void sc_detect_thread_stop()
     TRC("Enter\n");
 
     // cancel waiting SCardEstablishContext() inside the thread
-    if (l_context) {
-        LONG rv = SCardCancel(l_context);
+    if (detect_context) {
+        LONG rv = SCardCancel(detect_context);
         CHECK("SCardCancel", rv);
     }
-    pthread_join(l_thread_id, NULL);
+    pthread_join(detect_thread_id, NULL);
 
     TRC("Leave\n");
 }
